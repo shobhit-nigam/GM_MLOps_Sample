2f03aefcce9aa4c1937fce1a58fae7710a6b72bf
shobhit-nigam <shobhit-nigam>
2024-06-12 19:29:25 +0000

    git commit

---
+++ sample_git.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+print("hello world")
+
+# COMMAND ----------
+
+secret_value = dbutils.secrets.get(scope="trial", key="our-key") 
+
+# COMMAND ----------
+
+print(secret_value)
+
+# COMMAND ----------
+
+
